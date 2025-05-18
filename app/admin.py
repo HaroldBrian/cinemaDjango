80606c80f77a42369058cc93351c5e633ae57893
HaroldBrian <89621396+HaroldBrian@users.noreply.github.com>
2025-05-18 17:53:22 +0200

    feature:Add rating filter to Movie admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Author, Movie
 
-# Register your models here.
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Évaluation'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            (1, '1'),
+            (2, '2'),
+            (3, '3'),
+            (4, '4'),
+            (5, '5'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(rating=self.value())
+        return queryset
+
 class MovieAdmin(admin.ModelAdmin):
-    list_filter = ['release_date']
+    list_filter = ['release_date', RatingFilter]
     list_display = ['title', 'description', 'release_date', 'rating', 'author', 'status']
 
 admin.site.register(Author)
