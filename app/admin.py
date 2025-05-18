5decf0f980a7550371987316189a82e1147f1f61
HaroldBrian <89621396+HaroldBrian@users.noreply.github.com>
2025-05-18 17:44:26 +0200

    feature:Register Movie and Author models and add show differents columns

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Author, Movie
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_filter = ['release_date']
+    list_display = ['title', 'description', 'release_date', 'rating', 'author', 'status']
+
 admin.site.register(Author)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
