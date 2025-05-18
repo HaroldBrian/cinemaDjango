ab14695d7829a4f5ec4ad9669887fda41c0bf40c
HaroldBrian <89621396+HaroldBrian@users.noreply.github.com>
2025-05-18 18:01:53 +0200

    feature:Add status filter to Movie admin

--- app/admin.py
+++ app/admin.py
@@ -19,8 +19,20 @@ class RatingFilter(admin.SimpleListFilter):
             return queryset.filter(rating=self.value())
         return queryset
 
+class StatusFilter(admin.SimpleListFilter):
+    title = 'Statut'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return Movie.STATUS_CHOICES
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(status=self.value())
+        return queryset
+
 class MovieAdmin(admin.ModelAdmin):
-    list_filter = ['release_date', RatingFilter]
+    list_filter = ['release_date', RatingFilter, StatusFilter]
     list_display = ['title', 'description', 'release_date', 'rating', 'author', 'status']
 
 admin.site.register(Author)
